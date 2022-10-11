78e54ad87cd2aff7ca652714ed8268d18e84f6ac
HimanshuV2208 <him2282001@gmail.com>
2022-10-12 00:44:39 +0530

    Added Backgrounds to Pokemon Type.

--- app/src/main/java/com/example/pokedex/PokemonActivity.java
+++ app/src/main/java/com/example/pokedex/PokemonActivity.java
@@ -1,8 +1,15 @@
 package com.example.pokedex;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.BackgroundColorSpan;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -72,19 +79,16 @@ public class PokemonActivity extends AppCompatActivity {
                             .resize(256, 256).centerCrop().into(imageView);
                     JSONArray types = response.getJSONArray("types");
                     JSONArray moves = response.getJSONArray("moves");
-                    String finalType="";
+                    String type1 = "", type2 = "";
                     for(int i=0; i<types.length();i++){
                         JSONObject typeEntry = types.getJSONObject(i);
                         int slot = typeEntry.getInt("slot");
                         String type = typeEntry.getJSONObject("type").getString("name");
                         type = type.substring(0, 1).toUpperCase() + type.substring(1);
-                        if(slot==1){
-                            finalType = type;
-                        }else if(slot==2){
-                            finalType = finalType.concat(" & ".concat(type));
-                        }
-                        setTypeText(finalType);
+                        if(slot==1) type1 = type;
+                        else if(slot==2) type2 = type;
                     }
+                    setTypeText(type1, type2);
                     for (TextView textView : Arrays.asList(mov1, mov2, mov3, mov4)) {
                         int movNo = random.nextInt(moves.length());
                         String move = moves.getJSONObject(movNo).getJSONObject("move").getString("name");
@@ -103,8 +107,44 @@ public class PokemonActivity extends AppCompatActivity {
         requestQueue.add(request);
     }
 
-    private void setTypeText(String type){
-        type1TxtV.setText(type);
+    private void setTypeText(String type1, String type2){
+        BackgroundColorSpan bcs1 = getBackground(type1), bcs2;
+        SpannableStringBuilder ssb = new SpannableStringBuilder();
+        ssb.append(type1);
+        if(type2.equals("")) {
+            bcs2 = null;
+        }
+        else {
+            bcs2 = getBackground(type2);
+            ssb.append(" & ");
+            ssb.append(type2);
+        }
+        ssb.setSpan(bcs1, 0, type1.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        if(bcs2!=null) ssb.setSpan(bcs2, type1.length()+3, type1.length()+3+type2.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        type1TxtV.setText(ssb);
+    }
+
+    private BackgroundColorSpan getBackground(String type){
+        BackgroundColorSpan backgroundColorSpan;
+        int color = 0, col;
+        if(type.equalsIgnoreCase("ghost")) color = R.color.ghost;
+        else if(type.equalsIgnoreCase("dragon")) color = R.color.dragon;
+        else if(type.equalsIgnoreCase("ice")) color = R.color.ice;
+        else if(type.equalsIgnoreCase("fighting")) color = R.color.fighting;
+        else if(type.equalsIgnoreCase("electric")) color = R.color.electric;
+        else if(type.equalsIgnoreCase("rock")) color = R.color.rock;
+        else if(type.equalsIgnoreCase("bug")) color = R.color.bug;
+        else if(type.equalsIgnoreCase("fire")) color = R.color.fire;
+        else if(type.equalsIgnoreCase("grass")) color = R.color.grass;
+        else if(type.equalsIgnoreCase("ground")) color = R.color.ground;
+        else if(type.equalsIgnoreCase("psychic")) color = R.color.psychic;
+        else if(type.equalsIgnoreCase("flying")) color = R.color.flying;
+        else if(type.equalsIgnoreCase("normal")) color = R.color.normal;
+        else if(type.equalsIgnoreCase("water")) color = R.color.water;
+        else if(type.equalsIgnoreCase("poison")) color = R.color.poison;
+        int c = getResources().getColor(color);
+        backgroundColorSpan = new BackgroundColorSpan(c);
+        return backgroundColorSpan;
     }
 
 }
